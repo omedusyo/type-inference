882afa8eab248a3881f7276399ac4b426a026aa3
Yuriy Dupyn <2153100+omedusyo@users.noreply.github.com>
2021-09-11 14:08:07 +0200

    Separate value/evaluation related stuff into its own module

---
+++ src/Evaluation.elm
@@ -0,0 +1,319 @@
+module Evaluation exposing (..)
+
+import AssocList exposing (Dict)
+import Main exposing (..)
+
+
+type Value
+    = -- ==Cartesian Product==
+      PairValue Value Value
+      -- ==Function Space==
+    | Closure { env : TermEnvironment, var : TermVarName, body : Term }
+      -- ==Coproduct==
+    | LeftValue Value
+    | RightValue Value
+      -- Booleans
+    | TrueValue
+    | FalseValue
+      --==Natural Number Object==
+    | NatValue NatValue
+
+
+type NatValue
+    = NatZeroValue
+    | NatSuccValue NatValue
+
+
+type EvalError
+    = UndefinedVar String
+    | ExpectedPair
+    | ExpectedFunction
+    | ExpectedLeftRight
+    | ExpectedBoolean
+    | ExpectedNat
+
+
+
+-- ===TERM ENVIRONMENT===
+
+
+type alias TermEnvironment =
+    -- We have `List Value` instead of `Value` because of shadowing of variables
+    Dict TermVarName (List Value)
+
+
+emptyTermEnvironment =
+    AssocList.empty
+
+
+lookupEnvironment : TermVarName -> TermEnvironment -> Maybe Value
+lookupEnvironment varName env =
+    case AssocList.get varName env of
+        Just terms ->
+            case terms of
+                [] ->
+                    Nothing
+
+                term0 :: _ ->
+                    Just term0
+
+        Nothing ->
+            Nothing
+
+
+extendEnvironment : TermVarName -> Value -> TermEnvironment -> TermEnvironment
+extendEnvironment varName term env =
+    AssocList.update varName
+        (\maybeBinding ->
+            case maybeBinding of
+                Just terms ->
+                    Just (term :: terms)
+
+                Nothing ->
+                    Just [ term ]
+        )
+        env
+
+
+
+-- ===EVALUATION===
+
+
+eval : TermEnvironment -> Term -> Result (List EvalError) Value
+eval env term =
+    case term of
+        VarUse varName ->
+            case lookupEnvironment varName env of
+                Just result ->
+                    Ok result
+
+                Nothing ->
+                    Err [ UndefinedVar varName ]
+
+        Pair fst snd ->
+            let
+                evaledFstResult =
+                    eval env fst
+
+                evaledSndResult =
+                    eval env snd
+            in
+            -- TODO: you should evaluate the first component, and if the first component is error, then short circuit and don't eval second component
+            -- TODO: I guess there are multiple possibilities...?
+            case ( evaledFstResult, evaledSndResult ) of
+                ( Ok evaledFst, Ok evaledSnd ) ->
+                    Ok (PairValue evaledFst evaledSnd)
+
+                ( Err errFst, Ok evaledSnd ) ->
+                    Err errFst
+
+                ( Ok evaledFst, Err errSnd ) ->
+                    Err errSnd
+
+                ( Err errFst, Err errSnd ) ->
+                    Err (errFst ++ errSnd)
+
+        Fst term1 ->
+            let
+                evaledTermResult =
+                    eval env term1
+            in
+            case evaledTermResult of
+                Ok evaledTerm ->
+                    case evaledTerm of
+                        PairValue fst _ ->
+                            Ok fst
+
+                        _ ->
+                            Err [ ExpectedPair ]
+
+                Err err ->
+                    Err err
+
+        Snd term1 ->
+            let
+                evaledTermResult =
+                    eval env term1
+            in
+            case evaledTermResult of
+                Ok evaledTerm ->
+                    case evaledTerm of
+                        PairValue _ snd ->
+                            Ok snd
+
+                        _ ->
+                            Err [ ExpectedPair ]
+
+                Err err ->
+                    Err err
+
+        Abstraction var body ->
+            Ok (Closure { env = env, var = var, body = body })
+
+        Application fn arg ->
+            let
+                fnEvaledResult =
+                    eval env fn
+            in
+            case fnEvaledResult of
+                Ok (Closure ({ var, body } as closure)) ->
+                    let
+                        argEvaledResult =
+                            eval env arg
+                    in
+                    case argEvaledResult of
+                        Ok argEvaled ->
+                            let
+                                newEnv =
+                                    extendEnvironment var argEvaled closure.env
+                            in
+                            eval newEnv body
+
+                        Err err ->
+                            Err err
+
+                Ok _ ->
+                    Err [ ExpectedFunction ]
+
+                Err err ->
+                    Err err
+
+        Left term1 ->
+            let
+                evaledTermResult =
+                    eval env term1
+            in
+            case evaledTermResult of
+                Ok evaledTerm ->
+                    Ok (LeftValue evaledTerm)
+
+                Err err ->
+                    Err err
+
+        Right term1 ->
+            let
+                evaledTermResult =
+                    eval env term1
+            in
+            case evaledTermResult of
+                Ok evaledTerm ->
+                    Ok (RightValue evaledTerm)
+
+                Err err ->
+                    Err err
+
+        Case { arg, leftVar, leftBody, rightVar, rightBody } ->
+            let
+                argEvaledResult =
+                    eval env arg
+            in
+            case argEvaledResult of
+                Ok argEvaled ->
+                    case argEvaled of
+                        LeftValue val ->
+                            let
+                                newEnv =
+                                    extendEnvironment leftVar val env
+                            in
+                            eval newEnv leftBody
+
+                        RightValue val ->
+                            let
+                                newEnv =
+                                    extendEnvironment rightVar val env
+                            in
+                            eval newEnv rightBody
+
+                        _ ->
+                            Err [ ExpectedLeftRight ]
+
+                Err errs ->
+                    Err errs
+
+        BoolTrue ->
+            Ok TrueValue
+
+        BoolFalse ->
+            Ok FalseValue
+
+        IfThenElse arg leftBody rightBody ->
+            let
+                argEvaledResult =
+                    eval env arg
+            in
+            case argEvaledResult of
+                Ok argEvaled ->
+                    case argEvaled of
+                        TrueValue ->
+                            eval env leftBody
+
+                        FalseValue ->
+                            eval env rightBody
+
+                        _ ->
+                            Err [ ExpectedBoolean ]
+
+                Err errs ->
+                    Err errs
+
+        NatZero ->
+            Ok (NatValue NatZeroValue)
+
+        NatSucc term1 ->
+            let
+                term1EvaledResult =
+                    eval env term1
+            in
+            case term1EvaledResult of
+                Ok argEvaled ->
+                    case argEvaled of
+                        NatValue natVal ->
+                            Ok (NatValue (NatSuccValue natVal))
+
+                        _ ->
+                            Err [ ExpectedNat ]
+
+                Err errs ->
+                    Err errs
+
+        NatLoop { base, loop, arg } ->
+            -- TODO: error on same var name? loop.indexVar, loop.stateVar
+            let
+                argEvaledResult =
+                    eval env arg
+            in
+            case argEvaledResult of
+                Ok argEvaled ->
+                    case argEvaled of
+                        NatValue natVal ->
+                            let
+                                evalNatLoop natVal0 =
+                                    case natVal0 of
+                                        NatZeroValue ->
+                                            eval env base
+
+                                        NatSuccValue natVal1 ->
+                                            let
+                                                prevResult =
+                                                    evalNatLoop natVal1
+                                            in
+                                            case prevResult of
+                                                Ok prevVal ->
+                                                    let
+                                                        newEnv =
+                                                            env
+                                                                |> extendEnvironment loop.indexVar (NatValue natVal1)
+                                                                |> extendEnvironment loop.stateVar prevVal
+                                                    in
+                                                    eval newEnv loop.body
+
+                                                Err errs ->
+                                                    Err errs
+                            in
+                            evalNatLoop natVal
+
+                        _ ->
+                            Err [ ExpectedNat ]
+
+                Err errs ->
+                    Err errs
--- src/Show.elm
+++ src/Show.elm
@@ -1,7 +1,153 @@
 module Show exposing (..)
 
 import AssocList exposing (Dict)
+import Evaluation exposing (..)
 import Main exposing (..)
+
+
+
+-- ===VALUES===
+
+
+showTerm : Term -> String
+showTerm term =
+    case term of
+        VarUse varname ->
+            String.concat [ "$", varname ]
+
+        Pair fst snd ->
+            String.concat [ "(", showTerm fst, ", ", showTerm snd, ")" ]
+
+        Fst term1 ->
+            String.concat [ showTerm term1, ".0" ]
+
+        Snd term1 ->
+            String.concat [ showTerm term1, ".1" ]
+
+        Abstraction var body ->
+            String.concat [ "lam(", var, " -> ", showTerm body, ")" ]
+
+        Application fn arg ->
+            String.concat [ "[", showTerm fn, " ", showTerm arg, "]" ]
+
+        Left term1 ->
+            String.concat [ "L(", showTerm term1, ")" ]
+
+        Right term1 ->
+            String.concat [ "R(", showTerm term1, ")" ]
+
+        Case { arg, leftVar, leftBody, rightVar, rightBody } ->
+            String.concat
+                [ "case("
+                , showTerm arg
+                , ", Left "
+                , leftVar
+                , " -> "
+                , showTerm leftBody
+                , ", Right "
+                , rightVar
+                , " -> "
+                , showTerm rightBody
+                , ")"
+                ]
+
+        BoolTrue ->
+            "True"
+
+        BoolFalse ->
+            "False"
+
+        IfThenElse arg leftBody rightBody ->
+            String.concat
+                [ "if("
+                , showTerm arg
+                , " then "
+                , showTerm leftBody
+                , " else "
+                , showTerm rightBody
+                , ")"
+                ]
+
+        NatZero ->
+            "Z"
+
+        NatSucc term1 ->
+            String.concat [ "S(", showTerm term1, ")" ]
+
+        NatLoop { base, loop, arg } ->
+            String.concat
+                [ "loop("
+                , loop.stateVar
+                , " := "
+                , showTerm base
+                , ", for "
+                , loop.indexVar
+                , " in range("
+                , showTerm arg
+                , ") do "
+                , loop.stateVar
+                , " := "
+                , showTerm loop.body
+                , ")"
+                ]
+
+
+showTermEnvironment : TermEnvironment -> String
+showTermEnvironment env =
+    env
+        |> AssocList.toList
+        |> List.concatMap
+            (\( varname, vals ) ->
+                case List.head vals of
+                    Just val ->
+                        [ String.concat [ varname, " := ", showValue val ] ]
+
+                    Nothing ->
+                        []
+            )
+        |> String.join ", "
+
+
+showEval : TermEnvironment -> Term -> Result (List EvalError) String
+showEval env term =
+    eval env term
+        |> Result.map
+            (\val ->
+                String.concat [ showTerm term, "  ~~>  ", showValue val ]
+            )
+
+
+
+-- ===TYPES===
+
+
+showType : Type -> String
+showType type0 =
+    case type0 of
+        VarType n ->
+            String.concat [ "'", String.fromInt n ]
+
+        Product type1 type2 ->
+            String.concat
+                [ "(", showType type1, " , ", showType type2, ")" ]
+
+        Sum type1 type2 ->
+            String.concat
+                [ "[", showType type1, " + ", showType type2, "]" ]
+
+        Arrow type1 type2 ->
+            String.concat
+                [ "(", showType type1, " -> ", showType type2, ")" ]
+
+        LambdaBool ->
+            "Bool"
+
+        LambdaNat ->
+            "Nat"
+
+
+
+-- ===INFERENCE===
 
 
 showInfer0 : Term -> Result (List TypeError) String
@@ -43,7 +189,7 @@ showValue val =
         Closure { env, var, body } ->
             String.concat
                 [ "{"
-                , showEnvironment env
+                , showTermEnvironment env
                 , if AssocList.isEmpty env then
                     "lam("
 
@@ -93,28 +239,3 @@ showNatValue natVal =
     --         String.concat [ "S(", showNatValue val1, ")" ]
     natValToInt natVal
         |> String.fromInt
-
-
-showEnvironment : TermEnvironment -> String
-showEnvironment env =
-    env
-        |> AssocList.toList
-        |> List.concatMap
-            (\( varname, vals ) ->
-                case List.head vals of
-                    Just val ->
-                        [ String.concat [ varname, " := ", showValue val ] ]
-
-                    Nothing ->
-                        []
-            )
-        |> String.join ", "
-
-
-showEval : TermEnvironment -> Term -> Result (List EvalError) String
-showEval env term =
-    eval env term
-        |> Result.map
-            (\val ->
-                String.concat [ showTerm term, "  ~~>  ", showValue val ]
-            )
