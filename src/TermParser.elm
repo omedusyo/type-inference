763a5d5e915aa722c26b1bd1df160d8de59a0a18
Yuriy Dupyn <2153100+omedusyo@users.noreply.github.com>
2021-09-11 22:16:15 +0200

    Implement rudementary variable parsing

--- src/TermParser.elm
+++ src/TermParser.elm
@@ -2,6 +2,55 @@ module TermParser exposing (..)
 
 import Main exposing (..)
 import Parser exposing ((|.), (|=), Parser)
+import Set
+
+
+
+-- ===VAR===
+
+
+varIntro : Parser TermVarName
+varIntro =
+    let
+        excludedChars =
+            Set.fromList [ '$', '(', ')', '{', '}', ' ', '\n', '\t', '\'', '"' ]
+
+        isPrintable charCode =
+            32 <= charCode && charCode <= 126
+    in
+    Parser.variable
+        { start =
+            -- TODO: first attempt
+            \c ->
+                let
+                    charCode =
+                        Char.toCode c
+                in
+                not (Set.member c excludedChars)
+                    && isPrintable charCode
+                    && not (Char.isDigit c)
+
+        -- TODO also filter-out the non-printable characters and whitespace
+        , inner =
+            \c ->
+                -- TODO: the only difference from start is can actually be a digit
+                let
+                    charCode =
+                        Char.toCode c
+                in
+                not (Set.member c excludedChars)
+                    && isPrintable charCode
+        , reserved =
+            -- TODO
+            Set.empty
+        }
+
+
+varUse : Parser Term
+varUse =
+    Parser.succeed VarUse
+        |. Parser.symbol "$"
+        |= varIntro
 
 
 
