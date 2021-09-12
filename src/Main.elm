6b7a21f7126c3ac84912a8ca6b6dd59c07e8070e
Yuriy Dupyn <2153100+omedusyo@users.noreply.github.com>
2021-09-12 19:31:37 +0200

    Add elm-ui

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,15 @@
 module Main exposing (..)
 
-import Evaluation
+import Element as E exposing (Element)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Evaluation as L
 import Html as H
-import Inference
-import LambdaBasics
+import Inference as L
+import LambdaBasics as L
 import Return exposing (Return)
-import TermParser
+import TermParser as L
 
 
 type alias Model =
@@ -23,4 +27,19 @@ update msg model =
 
 
 main =
-    H.text "Hello!"
+    E.layout []
+        (E.row [ E.width E.fill, E.centerX ]
+            [ e
+            , e
+            ]
+        )
+
+
+e =
+    E.el
+        [ Background.color (E.rgb255 240 0 245)
+        , Font.color (E.rgb255 255 255 255)
+        , Border.rounded 3
+        , E.padding 30
+        ]
+        (E.text "hello, world!")
