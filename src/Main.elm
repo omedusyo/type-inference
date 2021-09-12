dfe487bcf356fda0ef481167221ece2b014fd073
Yuriy Dupyn <2153100+omedusyo@users.noreply.github.com>
2021-09-12 19:10:45 +0200

    Initial commit

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,26 @@
 module Main exposing (..)
 
+import Evaluation
+import Html as H
+import Inference
+import LambdaBasics
+import Return exposing (Return)
+import TermParser
 
-dasdasdsds =
-    3
+
+type alias Model =
+    {}
+
+
+type Msg
+    = NothinYet
+
+
+update : Msg -> Model -> Return Msg Model
+update msg model =
+    model
+        |> Return.singleton
+
+
+main =
+    H.text "Hello!"
