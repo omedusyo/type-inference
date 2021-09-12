9a628511a42e93b2ee28c17b8c23f3b648c0ce2f
Yuriy Dupyn <2153100+omedusyo@users.noreply.github.com>
2021-09-12 13:08:44 +0200

    Implement basic parsing for literals/simply operators

--- src/TermParser.elm
+++ src/TermParser.elm
@@ -16,6 +16,45 @@ spaces =
 
 
 
+-- ===TERM===
+
+
+term : Parser Term
+term =
+    Parser.oneOf
+        [ -- literals/variables
+          varUse
+        , emptyList
+        , bool
+        , natConstant -- TODO: this has to be last constant? why? Does it chomp stuff?
+
+        -- operatorTerm has to be at the ned
+        , operatorTerm
+        ]
+
+
+operatorTerm : Parser Term
+operatorTerm =
+    -- TODO: how to refactor this identity?
+    Parser.succeed (\x -> x)
+        |. Parser.symbol "("
+        |. spaces
+        |= Parser.oneOf
+            [ abstraction
+            , ifThenElse
+            , pair
+            , snd
+            , fst
+            , left
+            , right
+            , natSucc
+            , cons
+            ]
+        |. Parser.symbol ")"
+        |. spaces
+
+
+
 -- ===VAR===
 
 
@@ -72,13 +111,13 @@ varUse =
 true : Parser Term
 true =
     Parser.succeed BoolTrue
-        |. Parser.keyword "True"
+        |. Parser.keyword "true"
 
 
 false : Parser Term
 false =
     Parser.succeed BoolFalse
-        |. Parser.keyword "False"
+        |. Parser.keyword "false"
 
 
 bool : Parser Term
@@ -86,8 +125,101 @@ bool =
     Parser.oneOf [ true, false ]
 
 
+ifThenElse : Parser Term
+ifThenElse =
+    Parser.succeed IfThenElse
+        |. Parser.keyword "if"
+        |. spaces
+        |= Parser.lazy (\() -> term)
+        |. spaces
+        |= Parser.lazy (\() -> term)
+        |. spaces
+        |= Parser.lazy (\() -> term)
+        |. spaces
 
--- TODO if-then-else
+
+
+-- ==Cartesian Product==
+-- (pair e e')
+
+
+pair : Parser Term
+pair =
+    Parser.succeed Pair
+        |. Parser.keyword "pair"
+        |. spaces
+        |= Parser.lazy (\() -> term)
+        |. spaces
+        |= Parser.lazy (\() -> term)
+        |. spaces
+
+
+fst : Parser Term
+fst =
+    Parser.succeed Fst
+        |. Parser.keyword "first"
+        |. spaces
+        |= Parser.lazy (\() -> term)
+        |. spaces
+
+
+snd : Parser Term
+snd =
+    Parser.succeed Snd
+        |. Parser.keyword "second"
+        |. spaces
+        |= Parser.lazy (\() -> term)
+        |. spaces
+
+
+
+-- ==Function Space==
+-- TODO: generalize so it can take 0,1,2,... variables
+-- (fn (x) body)
+
+
+abstraction : Parser Term
+abstraction =
+    Parser.succeed Abstraction
+        |. Parser.keyword "fn"
+        |. spaces
+        -- Parameters
+        |. Parser.symbol "("
+        |. spaces
+        |= varIntro
+        |. spaces
+        |. Parser.symbol ")"
+        |. spaces
+        -- body
+        |= Parser.lazy (\() -> term)
+        |. spaces
+
+
+
+-- TODO: application
+-- ==Coproduct==
+
+
+left : Parser Term
+left =
+    Parser.succeed Left
+        |. Parser.keyword "left"
+        |. spaces
+        |= Parser.lazy (\() -> term)
+        |. spaces
+
+
+right : Parser Term
+right =
+    Parser.succeed Right
+        |. Parser.keyword "right"
+        |. spaces
+        |= Parser.lazy (\() -> term)
+        |. spaces
+
+
+
+-- TODO: case
 -- ===Nat===
 
 
@@ -97,5 +229,36 @@ natConstant =
         |> Parser.map intToNatTerm
 
 
+natSucc : Parser Term
+natSucc =
+    Parser.succeed NatSucc
+        |. Parser.keyword "succ"
+        |. spaces
+        |= Parser.lazy (\() -> term)
+        |. spaces
+
+
 
 -- TODO: nat-loop
+-- ===Lists===
+
+
+emptyList : Parser Term
+emptyList =
+    Parser.succeed EmptyList
+        |. Parser.keyword "empty-list"
+
+
+cons : Parser Term
+cons =
+    Parser.succeed Cons
+        |. Parser.keyword "cons"
+        |. spaces
+        |= Parser.lazy (\() -> term)
+        |. spaces
+        |= Parser.lazy (\() -> term)
+        |. spaces
+
+
+
+-- TODO: list-loop
